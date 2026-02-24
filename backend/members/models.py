f868f40e00cbe9289de45a7841874b4714ad2773
Jerome Maunga <jerhomemaunga@gmail.com>
2026-02-24 18:24:05 +0300

    Initialized the members' attributes

--- backend/members/models.py
+++ backend/members/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    profile_picture = models.ImageField(upload_to='members/', null=True, blank=True)
+    join_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+        
