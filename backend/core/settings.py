c5e3b28acf23f5e6b2c54f7378b78a7ee836b457
Jerome Maunga <jerhomemaunga@gmail.com>
2026-02-24 18:03:23 +0300

    Updated the settings by adding the python-dotenv

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG')=='True'
