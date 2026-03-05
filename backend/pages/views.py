f6fa29280d5a981e407f9b800c9ebf8ca33c0967
Jerome Maunga <jerhomemaunga@gmail.com>
2026-03-05 13:41:12 +0300

    Established the model views of the Leadership page

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from leadership.models import Leader
+from members.models import Member
 
 # Create your views here.
+
+def home(request):
+    leaders = Leader.objects.all()
+    members = Member.objects.all()[:5]
+
+    context = {
+        'leaders' : leaders,
+        'members' : members
+    }
+
+    return render(request, 'pages/home.html', context)
