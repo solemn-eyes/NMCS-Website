72ddfd7fa0413c788a37305817ff8fc6c4f4e72b
Jerome Maunga <jerhomemaunga@gmail.com>
2026-03-05 13:42:13 +0300

    Created the leadership entity with its attributes

--- backend/leadership/models.py
+++ backend/leadership/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Leader(models.Model):
+    full_name = models.CharField(max_length=150)
+    position = models.CharField(max_length=100)
+    message = models.TextField()
+    profile_picture = models.ImageField(upload_to='leaders/')
+
+    def __str__(self):
+        return f"{self.full_name} - {self.position}"
+        
