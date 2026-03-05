2b5c8c4b8082b873666edaf5a1b1386c818b1819
Jerome Maunga <jerhomemaunga@gmail.com>
2026-03-05 14:37:58 +0300

    Made additional modifications and migrations

--- backend/gallery/models.py
+++ backend/gallery/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class GalleryImage(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='gallery/')
+    uploaded_at = models.DateTimeField(auto_now_add= True)
+
+    def __str__(self):
+        return self.title
+
--- backend/leadership/views.py
+++ backend/leadership/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Leader
 # Create your views here.
+
+def leadership_list(request):
+    leaders = Leader.objects.all()
+    return render(request, 'leadership/list.html', {'leaders': leaders})
+    
